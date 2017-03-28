7a4fc515f6b7010e0699d3f9f34cc9c723ae86b4
Student <student@macbook-pro-1174-corley.local>
2017-03-28 17:15:40 -0400

    finished

--- Recap.playground/Pages/Iteration and Conditionals.xcplaygroundpage/Contents.swift
+++ Recap.playground/Pages/Iteration and Conditionals.xcplaygroundpage/Contents.swift
@@ -23,6 +23,25 @@ let canvas = Canvas(width: 400, height: 400)
 
 // Replace this comment and add your code below
 
+canvas.drawShapesWithBorders = false
+
+canvas.translate(byX: 200, byY: 200)
+
+for x in stride(from: 0, through: 360, by: 36){
+canvas.rotate(by: 20)
+canvas.fillColor = Color(hue: x, saturation: 100, brightness: 100, alpha: 50)
+canvas.drawEllipse(centreX: 0, centreY: 0, width: 20, height: 300)
+    
+}
+
+canvas.rotate(by: 10)
+
+for x in stride(from: 0, through: 360, by: 36){
+canvas.rotate(by: 20)
+canvas.fillColor = Color(hue: x, saturation:100, brightness: 100, alpha: 50)
+canvas.drawEllipse(centreX: 0, centreY: 0, width: 20, height: 200)
+
+}
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
